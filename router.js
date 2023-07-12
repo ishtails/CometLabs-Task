e805c96267042f84b9473f9aaa9e4dc9e46fd67b
Styles <bcs_2021035@iiitm.ac.in>
2023-07-12 16:38:04 +0530

    added axios

--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-import { registerUser, loginUser } from "./controllers/auth";
+import { registerUser, loginUser } from "./controllers/auth.js";
 
 import { Router } from "express";
 const router = Router();
@@ -8,15 +8,15 @@ router.post("/login", loginUser);
 router.post("/register", registerUser);
 
 //ADMIN
-router.post("/admin/addQ", addQuestion);
-router.patch("/admin/editQ/:id", editQuestion);
-router.delete("/admin/deleteQ/:id", deleteQuestion);
+// router.post("/admin/addQ", addQuestion);
+// router.patch("/admin/editQ/:id", editQuestion);
+// router.delete("/admin/deleteQ/:id", deleteQuestion);
 
-router.post("/admin/addT", addTestcase);
-router.patch("/admin/editT/:id", editTestcase);
-router.delete("/admin/deleteT/:id", deleteTestcase);
+// router.post("/admin/addT", addTestcase);
+// router.patch("/admin/editT/:id", editTestcase);
+// router.delete("/admin/deleteT/:id", deleteTestcase);
 
 //USER
-router.post("/user/check-answer/:id", checkSolution);
+// router.post("/user/check-answer/:id", checkSolution);
 
 export default router;
