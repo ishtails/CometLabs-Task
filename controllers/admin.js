1ac91541ac94ea5cbdc7306ec5ed34cd76c9e28c
Styles <bcs_2021035@iiitm.ac.in>
2023-07-12 22:26:55 +0530

    completed all admin controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,107 +1,193 @@
 import axios from "axios";
+import Joi from "joi";
+import problems from "../models/problemModel.js";
 
-export const addProblem = async () => {
-  const myProblem = {
-    name: "myProblem",
-    body: "Description of myProblem",
-    typeId: 0,
-    masterjudgeId: 1000,
-    interactive: false,
-  };
+export const addProblem = async (req, res) => {
+  try {
+    //Request Body Validation
+    const problemSchema = Joi.object({
+      name: Joi.string().required(),
+      body: Joi.string(),
+      typeId: Joi.number(),
+      masterjudgeId: Joi.number().required(),
+      interactive: Joi.boolean(),
+    });
+    await problemSchema.validateAsync(req.body);
 
-  const result = await axios.post(
-    `${process.env.SPHERE_ENDPOINT}/problems`,
-    myProblem,
-    {
-      params: {
-        access_token: process.env.SPHERE_PROBLEM_TOKEN,
-      },
-    }
-  );
-
-  console.log(result);
-};
-
-export const editProblem = async () => {
-  const problemId = 116534;
-
-  const updateObj = {
-    name: "myProblem_updated",
-  };
-
-  const result = await axios.put(
-    `${process.env.SPHERE_ENDPOINT}/problems/${problemId}`,
-    updateObj,
-    {
-      params: {
-        access_token: process.env.SPHERE_PROBLEM_TOKEN,
-      },
-    }
-  );
-
-  console.log(result);
-};
-
-export const deleteProblem = async () => {
-  const problemId = 116534;
-
-  const result = await axios.delete(
-    `${process.env.SPHERE_ENDPOINT}/problems/${problemId}`,
-    {
-      params: {
-        access_token: process.env.SPHERE_PROBLEM_TOKEN,
-      },
-    }
-  );
-
-  console.log(result);
-};
-
-export const addTestcase = async () => {
-  const problemId = 116534;
-
-  const myTestcase = {
-    input: "input_myTestcase_1",
-    output: "output_myTestcase_1",
-    timeLimit: 1,
-    judgeId: 3,
-    active: true,
-  };
-
-  const result = await axios.post(
-    `${process.env.SPHERE_ENDPOINT}/problems/${problemId}/testcases`,
-    myTestcase,
-    {
-      params: {
-        access_token: process.env.SPHERE_PROBLEM_TOKEN,
-      },
-    }
-  );
-
-  console.log(result);
-};
-
-export const updateTestcase = async () => {
-    const problemId = 116534;
-    const testcaseNumber = 0;
-  
-    const myTestcase = {
-      input: "input_myTestcase_1_updated",
-      output: "output_myTestcase_1_updated",
-      timeLimit: 2,
-      judgeId: 4,
-      active: false,
-    };
-  
-    const result = await axios.put(
-      `${process.env.SPHERE_ENDPOINT}/problems/${problemId}/testcases/${testcaseNumber}`,
-      myTestcase,
+    const result = await axios.post(
+      `${process.env.SPHERE_ENDPOINT}/problems`,
+      req.body,
       {
         params: {
           access_token: process.env.SPHERE_PROBLEM_TOKEN,
         },
       }
     );
-  
-    console.log(result);
-  };
+
+    // Save to DB
+    const problemId = result.data.id;
+    const newProblem = new problems({ ...req.body, problemId });
+    await newProblem.save();
+
+    return res
+      .status(200)
+      .send("Problem added with problemId: " + result.data.id);
+  } catch (error) {
+    if (error.details) {
+      return res
+        .status(422)
+        .json(error.details.map((detail) => detail.message).join(", "));
+    }
+    return res.status(500).send(error);
+  }
+};
+
+export const editProblem = async (req, res) => {
+  try {
+    const problemId = req.params.problemId;
+
+    if (!problemId) {
+      return res.status(400).send("No problemId provided");
+    }
+
+    //Request Body Validation
+    const problemSchema = Joi.object({
+      name: Joi.string(),
+      body: Joi.string(),
+      typeId: Joi.number(),
+      masterjudgeId: Joi.number(),
+      interactive: Joi.boolean(),
+    });
+    await problemSchema.validateAsync(req.body);
+
+    await axios.put(
+      `${process.env.SPHERE_ENDPOINT}/problems/${problemId}`,
+      req.body,
+      {
+        params: {
+          access_token: process.env.SPHERE_PROBLEM_TOKEN,
+        },
+      }
+    );
+
+    await problems.updateOne({ problemId }, { $set: req.body });
+
+    return res
+      .status(200)
+      .send("Problem with problemId: " + problemId + " updated");
+  } catch (error) {
+    if (error.details) {
+      return res
+        .status(422)
+        .json(error.details.map((detail) => detail.message).join(", "));
+    }
+    return res.status(500).send(error);
+  }
+};
+
+export const deleteProblem = async (req, res) => {
+  try {
+    const problemId = req.params.problemId;
+
+    await axios.delete(`${process.env.SPHERE_ENDPOINT}/problems/${problemId}`, {
+      params: {
+        access_token: process.env.SPHERE_PROBLEM_TOKEN,
+      },
+    });
+
+    await problems.deleteOne({ problemId });
+
+    return res
+      .status(200)
+      .send("Problem with problemId: " + problemId + " deleted");
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+export const addTestcase = async (req, res) => {
+  try {
+    const problemId = req.params.problemId;
+
+    //Request Body Validation
+    const registerSchema = Joi.object({
+      input: Joi.string(),
+      output: Joi.string(),
+      timeLimit: Joi.number(),
+      judgeId: Joi.number().required(),
+      active: Joi.boolean(),
+    });
+    await registerSchema.validateAsync(req.body);
+
+    const result = await axios.post(
+      `${process.env.SPHERE_ENDPOINT}/problems/${problemId}/testcases`,
+      req.body,
+      {
+        params: {
+          access_token: process.env.SPHERE_PROBLEM_TOKEN,
+        },
+      }
+    );
+
+    const testcaseId = result.data.number;
+    const testcases = { ...req.body, testcaseId };
+
+    await problems.updateOne({ problemId }, { $push: { testcases } });
+
+    return res
+      .status(200)
+      .send(`Testcase (${testcaseId}) added to problemId: ${problemId}`);
+  } catch (error) {
+    if (error.details) {
+      return res
+        .status(422)
+        .json(error.details.map((detail) => detail.message).join(", "));
+    }
+
+    return res.status(500).send(error);
+  }
+};
+
+export const editTestcase = async (req, res) => {
+  try {
+    const problemId = req.params.problemId;
+    const testcaseId = req.params.testcaseId;
+
+    //Request Body Validation
+    const registerSchema = Joi.object({
+      input: Joi.string().required(),
+      output: Joi.string().required(),
+      timeLimit: Joi.number().required(),
+      judgeId: Joi.number().required(),
+      active: Joi.boolean().required(),
+    });
+    await registerSchema.validateAsync(req.body);
+
+    await axios.put(
+      `${process.env.SPHERE_ENDPOINT}/problems/${problemId}/testcases/${testcaseId}`,
+      req.body,
+      {
+        params: {
+          access_token: process.env.SPHERE_PROBLEM_TOKEN,
+        },
+      }
+    );
+
+    await problems.updateOne(
+      { problemId, "testcases.testcaseId": testcaseId },
+      { $set: { "testcases.$": req.body } }
+    );
+
+    return res
+      .status(200)
+      .send(`Testcase (${testcaseId}) of problemId: ${problemId} updated`);
+  } catch (error) {
+    if (error.details) {
+      return res
+        .status(422)
+        .json(error.details.map((detail) => detail.message).join(", "));
+    }
+
+    return res.status(500).send(error);
+  }
+};
