9fa57071afd9fd355e3c94f112417b5184eac845
Styles <bcs_2021035@iiitm.ac.in>
2023-07-12 22:27:47 +0530

    updated problemModel to include testcases

--- models/problemModel.js
+++ models/problemModel.js
@@ -1,7 +1,41 @@
 import mongoose from "mongoose";
 
+const testcaseSchema = new mongoose.Schema({
+  testcaseId: {
+    type: Number,
+    required: [true, "Provide testcase ID"],
+  },
+
+  input: {
+    type: String,
+  },
+
+  output: {
+    type: String,
+  },
+
+  timeLimit: {
+    type: Number,
+  },
+
+  judgeId: {
+    type: Number,
+    required: [true, "Provide Judge ID"],
+  },
+  
+  active: {
+    type: Boolean,
+  },
+});
+
 const problemSchema = new mongoose.Schema(
   {
+    problemId: {
+      type: Number,
+      required: [true, "Provide Problem ID"],
+      unique: [true, "Email already exists"],
+    },
+
     name: {
       type: String,
       required: [true, "Provide Problem Name"],
@@ -23,6 +57,8 @@ const problemSchema = new mongoose.Schema(
     interactive: {
       type: Boolean,
     },
+
+    testcases: [testcaseSchema],
   },
   { timestamps: true }
 );
