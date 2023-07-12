e7688ad26aa560d20112f684ce8e0c606376e541
Styles <bcs_2021035@iiitm.ac.in>
2023-07-12 22:28:02 +0530

    updated router endpoints

--- router.js
+++ router.js
@@ -4,7 +4,7 @@ import {
   editProblem,
   deleteProblem,
   addTestcase,
-  updateTestcase,
+  editTestcase,
 } from "./controllers/admin.js";
 import {
   getAllProblems,
@@ -12,6 +12,7 @@ import {
   checkResult,
 } from "./controllers/users.js";
 import { Router } from "express";
+import { isAuth, isAdmin } from "./middlewares.js";
 const router = Router();
 
 //AUTH
@@ -19,15 +20,15 @@ router.post("/login", loginUser);
 router.post("/register", registerUser);
 
 //ADMIN
-router.post("/admin/add-problem", addProblem);
-router.put("/admin/edit-problem/:id", editProblem);
-router.delete("/admin/delete-problem/:id", deleteProblem);
-router.post("/admin/add-testcase/:problemId", addTestcase);
-router.put("/admin/update-testcase/:problemId/:testcaseId", updateTestcase);
+router.post("/admin/add-problem", isAuth, isAdmin, addProblem);
+router.put("/admin/edit-problem/:problemId", isAuth, isAdmin, editProblem);
+router.delete("/admin/delete-problem/:problemId", isAuth, isAdmin, deleteProblem);
+router.post("/admin/add-testcase/:problemId", isAuth, isAdmin, addTestcase);
+router.put("/admin/edit-testcase/:problemId/:testcaseId", isAuth, isAdmin, editTestcase);
 
 //USER
-router.get("/user/all-problems", getAllProblems);
-router.post("/user/submit-solution", createSubmission);
-router.get("/user/submissions/:id", checkResult);
+router.get("/user/all-problems", isAuth, getAllProblems);
+router.post("/user/submit-solution", isAuth, createSubmission);
+router.get("/user/submissions/:id", isAuth, checkResult);
 
 export default router;
